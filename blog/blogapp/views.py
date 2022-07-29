f3b1521e8899c53e0993c8ea056e8eee84ac3a62
KAPTOWE4KA <65016907+KAPTOWE4KA@users.noreply.github.com>
2022-07-29 17:54:28 +0600

    templates1

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def main_view(request):
+    posts = Post.objects.all()
+    return render(request, 'blogapp/index.html', context={'posts': posts})
+
+
+def create_post(request):
+    return render(request, 'blogapp/create.html', context={})
+    #TODO видео урок на 48:36. Продолжить дома
