35dbaa977c1216e804d76aa8ccdc425a5325009f
KAPTOWE4KA <65016907+KAPTOWE4KA@users.noreply.github.com>
2022-08-03 16:52:38 +0600

    django-lesson-complete

--- blog/blogapp/forms.py
+++ blog/blogapp/forms.py
@@ -4,5 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label='Имя')
     email = forms.EmailField(label='E-mail')
-    message = forms.Textarea(label='Сообщение')
-    #TODO 2:02:33 на видеоуроке
+    message = forms.CharField(label='Сообщение')
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Post
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 
 def main_view(request):
@@ -8,7 +12,19 @@ def main_view(request):
 
 
 def create_post(request):
-    return render(request, 'blogapp/create.html', context={})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            msg = form.cleaned_data['message']
+            email = form.cleaned_data['email']
+            send_mail('Contact message', msg, 'from@example.com', [email], fail_silently=True)
+            return HttpResponseRedirect(reverse('blog:main'))
+        else:
+            return render(request, 'blogapp/create.html', context={'form': form})
+    else:
+        form = ContactForm()
+        return render(request, 'blogapp/create.html', context={'form': form})
 
 
 def post(request, id):
